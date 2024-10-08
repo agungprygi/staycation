9ffb060c81da79b37524363a0a2fc955c4d20638
Agung Prayogi <agungprayogi305@gmail.com>
2024-10-08 10:34:14 +0700

    build all APIs

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,12 @@
-const router = require('express').Router();
-const apiControllers = require('../controllers/apiControllers');
-const { cloudinaryUpload, cloudinaryMultiUpload } = require('../middlewares/multer');
+const router = require("express").Router();
+const apiControllers = require("../controllers/apiControllers");
+const {
+  cloudinaryUpload,
+  cloudinaryMultiUpload,
+} = require("../middlewares/multer");
 
-router.get('/landing-page', apiControllers.landingPage);
+router.get("/landing-page", apiControllers.landingPage);
+router.get("/detail-page/:id", apiControllers.detailPage);
+router.post("/booking-page", cloudinaryUpload, apiControllers.bookingPage);
 
 module.exports = router;
